26bef53d491ed1cc25dffa4a4a1b8e4474bed833
unknown <you@example.com>
2023-06-08 11:49:20 +0600

    navbar and footer has been showed evrywhere except 404 page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,54 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
+                            {errors.email && <span 
+                            className="text-orange-500">cannot submit empty email </span>}
+                            
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" {...register("password",
+                                {
+                                    required: true,
+                                    minLength: 6,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/
+                                   
+
+                                })} placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === 'minLength' && <p className="text-orange-500">password is less than 6 characters</p>}
+                            {errors.password?.type === 'required' && <p className="text-orange-500">cannot submit empty password </p>}
+                            {errors.password?.type === 'pattern' && <p className="text-orange-500">password must have one uppercase and one special characters</p>}
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p>create a new account? <Link to="/register"> <u className="text-primary">Register</u></Link> </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,65 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Registration</h1>
+                </div>
+                <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">name</span>
+                            </label>
+                            <input type="text" {...register("name")} placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
+                            {errors.email && <span
+                                className="text-orange-500">cannot submit empty email </span>}
+
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">photo URL</span>
+                            </label>
+                            <input type="url" {...register("photo")} placeholder="photo URL" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" {...register("password",
+                                {
+                                    required: true,
+                                    minLength: 6,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/
+
+
+                                })} placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === 'minLength' && <p className="text-orange-500">password is less than 6 characters</p>}
+                            {errors.password?.type === 'required' && <p className="text-orange-500">cannot submit empty password </p>}
+                            {errors.password?.type === 'pattern' && <p className="text-orange-500">password must have one uppercase and one special characters</p>}
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p>already have an account? please <Link to="/login"> <u className="text-primary">login</u></Link> </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
